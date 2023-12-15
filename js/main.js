c7c5603715cdedadeee2de0413185fe7a3d59177
wendikardian <wendikardian@gmail.com>
2023-12-15 14:10:55 +0700

    nice

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var sales = new Vue({
     // create data product with array of object there is name shoes, price (in rupiah), price_after_discount, image
     shoes: [
       {
+        id: 1,
         name: "Nike Air Max 90",
         price: 2000000,
         price_after_discount: 1000000,
@@ -11,18 +12,21 @@ var sales = new Vue({
           "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/e835c4d8-c95a-4ddf-a9b6-86af099aeec0/air-max-tw-se-shoes-8CTsWh.png",
       },
       {
+        id: 2,
         name: "Nike Air Max 95",
         price: 3000000,
         price_after_discount: 1500000,
         image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/5ea4f44f-f320-463e-bc5a-f7daafe44d41/air-max-tw-shoes-DhpWvV.png",
       },
       {
+        id: 3,
         name: "Nike Air Max 97",
         price: 4000000,
         price_after_discount: 2000000,
         image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/746357f7-f971-45d4-ae1f-2fc59d3f2bf7/air-max-ap-shoes-3Rdq04.png",
       },
       {
+        id: 4,
         name: "Nike Air Max 98",
         price: 5000000,
         price_after_discount: 2500000,
@@ -35,13 +39,29 @@ var sales = new Vue({
         image: "https://hips.hearstapps.com/hmg-prod/images/nike-air-max-day-2023-640f5ee7d2778.jpg?crop=0.500xw:1.00xh;0.500xw,0&resize=640:*",
       },
       {
+        id: 5,
         name: "Nike Air Max 720",
         price: 7000000,
         price_after_discount: 3500000,
         image: "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/4ad1e0ea-0e52-4136-8dc0-271743a0c861/air-max-95-recraft-older-shoes-xdcRNM.png",
       },
     ],
-    title: "sales",
+    showModal: false,
+    selectedShoe: null,
+    quantity: 1,
   },
-  methods: {},
+  methods: {
+    toggleModal(shoe) {
+      this.selectedShoe = shoe;
+      this.showModal = !this.showModal;
+    },
+    increaseQuantity() {
+      this.quantity++;
+    },
+    decreaseQuantity() {
+      if (this.quantity > 1) {
+        this.quantity--;
+      }
+    }
+  },
 });
